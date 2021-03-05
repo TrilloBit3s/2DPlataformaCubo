b0d1b34bca6171bd2171623f0fe9891aa1ad034b
TrilloBit3s <TrilloBit3s@gmail.com>
2021-03-05 07:46:03 -0300

    First Commit

---
+++ Assets/Scritps/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿//Script para PC
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce; 
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        FixedUpdate();      
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(facingRight == false && moveInput > 0)
+        {   
+            Flip();
+        }else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    } 
+}
---
+++ Assets/Scritps/PlayerMoveJoystick.cs
@@ -0,0 +1,58 @@
+﻿// Scrip para Mobile
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveJoystick : MonoBehaviour
+{  
+    public float speed;
+    public float jumpForce; 
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+
+    //variaveis para joystick
+    private float horizontalMove = 0f;
+    public Joystick joystick;
+    public float runSpeedHorizontal = 2;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        FixedUpdate();      
+    }
+
+    void FixedUpdate()
+    {
+        horizontalMove = joystick.Horizontal * runSpeedHorizontal;
+        transform.position += new Vector3(horizontalMove, 0 , 0) * Time.deltaTime * speed;
+        
+        if(facingRight == false && horizontalMove > 0)
+        {   
+            Flip();
+        }else if(facingRight == true && horizontalMove < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    public void Jump()
+    {
+        if(rb.velocity.y == 0)
+        {
+          // rb.velocity = Vector2.up * jumpForce;
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    } 
+}
